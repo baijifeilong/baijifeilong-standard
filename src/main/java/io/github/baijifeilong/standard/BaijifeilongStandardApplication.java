27959ccd2193d590aff523d6a217596e5fd604c7
BaiJiFeiLong <baijifeilong@gmail.com>
2019-04-18 11:19:39 +0800

    Init project

---
+++ src/main/java/io/github/baijifeilong/standard/BaijifeilongStandardApplication.java
@@ -0,0 +1,13 @@
+package io.github.baijifeilong.standard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaijifeilongStandardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaijifeilongStandardApplication.class, args);
+	}
+
+}
