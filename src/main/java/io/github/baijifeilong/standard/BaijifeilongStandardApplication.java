c390114f0e4a13e253304e7429dbefbc976b1135
BaiJiFeiLong <baijifeilong@gmail.com>
2019-04-18 11:25:04 +0800

    $

--- src/main/java/io/github/baijifeilong/standard/BaijifeilongStandardApplication.java
+++ src/main/java/io/github/baijifeilong/standard/BaijifeilongStandardApplication.java
@@ -1,13 +1,9 @@
 package io.github.baijifeilong.standard;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class BaijifeilongStandardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BaijifeilongStandardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
 }
